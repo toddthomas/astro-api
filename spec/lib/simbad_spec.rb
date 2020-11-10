88c24c71fc59a66ebc129f2d80369d4366f922e0
Todd Thomas <toddthomas@acm.org>
2020-11-10 09:02:13 -0700

    Another `Simbad` spec.

--- spec/lib/simbad_spec.rb
+++ spec/lib/simbad_spec.rb
@@ -7,5 +7,11 @@ describe Simbad do
       expect(response.code).to eq 200
       expect(response.body).to match /Number of objects : 17/
     end
+
+    it 'succeeds with multiple constraints' do
+      response = subject.stars('Vmag<=2.0', 'rah >= 5.0', 'rah <= 6.0', 'dec >= -10.0', 'dec <= 10.0')
+      expect(response.code).to eq 200
+      expect(response.body).to match /Number of objects : 6/
+    end
   end
 end
