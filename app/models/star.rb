3beeab4dfc42b8da95abddfc9b3ea890d697538b
Todd Thomas <toddthomas@acm.org>
2020-11-09 17:18:35 -0700

    Validity check: now with more validity!

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,5 +1,5 @@
 class Star
-  attr_accessor :id
+  attr_accessor :id # Makes this class compatible with `ActiveRecord::Base`. Otherwise unused.
   attr_accessor :identifier
   attr_accessor :right_ascension
   attr_accessor :declination
@@ -8,12 +8,15 @@ class Star
 
   def ==(other)
     return true if other.equal?(self)
-    return false unless other.kind_of?(self.class)
+    return false unless other.is_a?(self.class)
 
     identifier == other.identifier
   end
 
   def valid?
-    identifier[0] == '*'
+    identifier[0] == '*' &&
+      (0.0..24.0).include?(right_ascension) &&
+      (-90.0..90.0).include?(declination) &&
+      visual_magnitude.is_a?(Numeric)
   end
 end
--- spec/models/star_spec.rb
+++ spec/models/star_spec.rb
@@ -1,21 +1,68 @@
 require 'rails_helper'
 
 describe Star do
-  context 'equality check' do
-    let(:deneb1) { build_stubbed(:alpha_cygni) }
-    let(:deneb2) { build_stubbed(:alpha_cygni) }
-    let(:betelgeuse) { build_stubbed(:betelgeuse) }
+  let(:deneb1) { build_stubbed(:alpha_cygni) }
+  let(:deneb2) { build_stubbed(:alpha_cygni) }
+  let(:betelgeuse) { build_stubbed(:betelgeuse) }
 
-    it 'true when different instances are logically equal' do
+  context 'equality check' do
+    it 'is true when different instances are logically equal' do
       expect(deneb1 == deneb2).to eq true
     end
 
-    it 'false when different instances aren\'t logically equal' do
+    it 'is false when different instances aren\'t logically equal' do
       expect(deneb1 == betelgeuse).to eq false
     end
 
-    it 'true when comparing same instance' do
+    it 'is true when comparing same instance' do
       expect(deneb2 == deneb2).to eq true
+    end
+  end
+
+  context 'validity check' do
+    let(:bad_identifier) do
+      betelgeuse.clone.tap do |bad|
+        bad.identifier = 'Bol D182'
+      end
+    end
+
+    let(:bad_ra) do
+      betelgeuse.tap do |bad|
+        bad.right_ascension = 25.134613
+      end
+    end
+
+    let(:bad_dec) do
+      betelgeuse.tap do |bad|
+        bad.declination = -345.161346
+      end
+    end
+
+    let(:bad_mag) do
+      betelgeuse.tap do |bad|
+        bad.visual_magnitude = '0.42'
+      end
+    end
+
+    it 'is true for valid instances' do
+      expect(deneb1.valid?).to eq true
+      expect(betelgeuse.valid?).to eq true
+    end
+
+    it 'is false for invalid identifier' do
+      expect(bad_identifier.valid?).to eq false
+    end
+
+    it 'is false for invalid RA' do
+      expect(bad_ra.valid?).to eq false
+    end
+
+    it 'is false for invalid DEC' do
+      expect(bad_dec.valid?).to eq false
+    end
+
+    it 'is false for invalid magnitude' do
+      expect(bad_mag.valid?).to eq false
     end
   end
 end
