c658063c618fa7fbd0ffeedd8f354acc2195a809
Todd Thomas <toddthomas@acm.org>
2020-11-09 17:24:15 -0700

    Improve naming of ra-dec parser specs.

--- spec/models/commands/simbad_ra_dec_parser_spec.rb
+++ spec/models/commands/simbad_ra_dec_parser_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
 describe Commands::SimbadRaDecParser do
-  context 'parsing right ascension' do
-    it 'succeeds' do
+  context 'parsing' do
+    it 'right ascension' do
       expect(Commands::SimbadRaDecParser.parse_ra(from: '03 08 10.1324535 +40 57 20.328013')).to eq 3.13614790375
     end
-  end
 
-  context 'declination is positive' do
-    it 'succeeds' do
+    it 'positive declination' do
       expect(Commands::SimbadRaDecParser.parse_dec(from: '03 08 10.1324535 +40 57 20.328013')).to eq 40.95564667027778
     end
-  end
 
-  context 'declination is negative' do
-    it 'succeeds' do
+    it 'negative declination' do
       expect(Commands::SimbadRaDecParser.parse_dec(from: '05 36 12.81335 -01 12 06.9089')).to eq -1.2019191388888888
     end
   end
